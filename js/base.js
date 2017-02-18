303aab4ebf45597d3b68d2d91b09f53358af614a
WenJenHsieh <tennis82517@frankdeMacBook-Pro.local>
2017-02-18 15:36:26 -0500

    minor

--- js/base.js
+++ js/base.js
@@ -1,9 +1,9 @@
 function render_side_bar(){
     //alert("render_side_bar");
     items=[{link:"index.html",class:"fa fa-fw fa-dashboard",name:"Dashboard"}];
-    items+=[{link:"course.html",class:"fa fa-graduation-cap",name:"Course"}]
+    items+=[{link:"course.html",class:"fa fa-graduation-cap",name:"Course"}];
     for (i in items){
-        $(side_bar).append("<li><a href=\""+items[i].link+\"><i class=\""+items[i].class+"\"></i>"+items[i].name+"</a></li>");
+        $(side_bar).append("<li><a href=\""+items[i].link+"\"><i class=\""+items[i].class+"\"></i>"+items[i].name+"</a></li>");
     }
 }
 window.onload=render_side_bar();
