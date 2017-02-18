f77f40c4bb0eb94f24a1a08e0c356f80426ace8a
WenJenHsieh <tennis82517@frankdeMacBook-Pro.local>
2017-02-18 15:28:23 -0500

    alert

--- js/base.js
+++ js/base.js
@@ -1,5 +1,5 @@
 function render_side_bar(){
-
+    alert("render_side_bar");
     $(side_bar).append("<li><a href=\"index.html\"><i class=\"fa fa-fw fa-dashboard\"></i> Dashboard</a></li>");
     $(side_bar).append("<li><a href=\"course.html\"><i class=\"fa fa-graduation-cap\"></i> Dashboard</a></li>");
 }
