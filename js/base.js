b1d2fee62ad9689912b7aa59b9d42a8be7308e02
WenJenHsieh <tennis82517@frankdeMacBook-Pro.local>
2017-02-18 15:34:48 -0500

    for loop

--- js/base.js
+++ js/base.js
@@ -1,6 +1,9 @@
 function render_side_bar(){
-    alert("render_side_bar");
-    $(side_bar).append("<li><a href=\"index.html\"><i class=\"fa fa-fw fa-dashboard\"></i> Dashboard</a></li>");
-    $(side_bar).append("<li><a href=\"course.html\"><i class=\"fa fa-graduation-cap\"></i> Dashboard</a></li>");
+    //alert("render_side_bar");
+    items=[{link:"index.html",class:"fa fa-fw fa-dashboard",name:"Dashboard"}];
+    items+=[{link:"course.html",class:"fa fa-graduation-cap",name:"Course"}]
+    for (i in items){
+        $(side_bar).append("<li><a href=\""+items[i].link+\"><i class=\""+items[i].class+"\"></i>"+items[i].name+"</a></li>");
+    }
 }
 window.onload=render_side_bar();
